e21d8abb43474e85103845bd98effc5758d50c26
Bsaltori <bisaltori@yahoo.com.br>
2020-07-28 12:27:21 -0300

    Exercicios
    If else, casa do Dário

---
+++ Exercicios_URI/URI_1047/URI_1047/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string[] num = Console.ReadLine().Split();
+            int horInicial = int.Parse(num[0]);
+            int minutoInicial = int.Parse(num[1]);
+            int horaFinal = int.Parse(num[2]);
+            int minutoFinal = int.Parse(num[3]);
+
+            int inicial = horInicial * 60 + minutoInicial;
+            int final = horaFinal * 60 + minutoFinal;
+
+            int duracao;
+            if (inicial < final)
+            {
+                duracao = final - inicial;
+            }
+            else
+            {
+                duracao = (24 * 60 - inicial) + final;
+            }
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHoras + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
+
+        }
+    }
+}
---
+++ Exercicios_URI/URI_1049/URI_1049/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace URI_1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String tipo1 = Console.ReadLine();
+            String tipo2 = Console.ReadLine();
+            String tipo3 = Console.ReadLine();
+
+            if (tipo1 == "vertebrado" && tipo2 == "ave" && tipo3 == "carnivoro")
+            {
+                Console.WriteLine("aguia");
+
+            } else if (tipo1 == "vertebrado" && tipo2 == "ave" && tipo3 == "onivoro")
+            {
+                Console.WriteLine("pomba");
+
+            } else if (tipo1 == "vertebrado" && tipo2 == "mamifero" && tipo3 == "onivoro")
+            {
+                Console.WriteLine("homem");
+
+            } else if (tipo1 == "vertebrado" && tipo2 == "mamifero" && tipo3 == "herbivoro")
+            {
+                Console.WriteLine("vaca");
+
+            }else if (tipo1 == "invertebrado" && tipo2 == "inseto" && tipo3 == "hematofago")
+            {
+                Console.WriteLine("pulga");
+
+            }else if (tipo1 == "invertebrado" && tipo2 == "inseto" && tipo3 == "herbivoro")
+            {
+                Console.WriteLine("lagarta");
+
+            }else if (tipo1 == "invertebrado" && tipo2 == "anelideo" && tipo3 == "hematofogo")
+            {
+                Console.WriteLine("sanguessuga");
+
+            }
+            else
+            {
+                Console.WriteLine("minhoca");
+            }
+
+        }
+
+    }
+}
