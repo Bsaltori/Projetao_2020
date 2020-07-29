7de87e1c11ea7641c48788369f1dc27d97e56fa0
Bsaltori <biasaltori@yahoo.com.br>
2020-07-29 17:30:42 -0300

    exercicios
    if else

---
+++ Exercicios_URI/URI_1045/URI_1045/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace URI_1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] triangulo = Console.ReadLine().Split();
+            double a = double.Parse(triangulo[0]);
+            double b = double.Parse(triangulo[1]);
+            double c = double.Parse(triangulo[2]);
+
+            double temp;
+
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            if (a < c)
+            {
+                temp = a;
+                a = c;
+                c = temp;
+            }
+            if (b < c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+           
+            if (a >= b + c)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            } 
+
+
+        }
+    }
+}
