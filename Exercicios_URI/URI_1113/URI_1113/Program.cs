7a5725dc3b17cf6f29f30c21e7a03fee080c5354
Bsaltori <biasaltori@yahoo.com.br>
2020-08-03 19:39:09 -0300

    Exercicio_While

---
+++ Exercicios_URI/URI_1113/URI_1113/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace URI_1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros = Console.ReadLine().Split(' ');
+            int x = int.Parse(numeros[0]);
+            int y = int.Parse(numeros[1]);
+
+            while (x != y)
+            {
+
+
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                numeros = Console.ReadLine().Split(' ');
+                x = int.Parse(numeros[0]);
+                y = int.Parse(numeros[1]);
+
+
+            }
+
+        }
+    }
+}
