f0d4e412e305a6f4b36ba68d5a4b25f59e0cfd68
Bsaltori <bisaltori@yahoo.com.br>
2020-09-06 13:10:27 -0300

    Exercicios repetição

---
+++ Exercicios_URI/URI_1067/URI_1067/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI_1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = int.Parse(Console.ReadLine());
+
+            for ()
+        }
+    }
+}
---
+++ Exercicios_URI/URI_1071/URI_1071/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace URI_1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, troca, soma = 0;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                troca = x;
+                x = y;
+                y = troca;
+            }
+
+            for (x += 1; x < y; x++)
+            {
+                if (x % 2 != 0)
+                {
+                    soma = soma + x;
+                }
+            }
+
+            Console.WriteLine(soma);
+
+
+
+
+
+        }
+    }
+}
