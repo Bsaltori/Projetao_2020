f66820b25d6179fab20ab045f0ebda9a76cd5989
Bsaltori <biasaltori@yahoo.com.br>
2020-09-24 11:21:12 -0300

    vetor

--- Exercicios_URI/URI_1117/URI_1117/Program.cs
+++ Exercicios_URI/URI_1117/URI_1117/Program.cs
@@ -6,7 +6,31 @@ namespace URI_1117
     {
         static void Main(string[] args)
         {
-            
+            double nAv1 = int.Parse(Console.ReadLine());
+
+
+            double media = 0.0;
+            while (nAv1 < 0.0 & nAv1 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                nAv1 = int.Parse(Console.ReadLine());
+
+            }
+            // calculo
+            double nAv2 = int.Parse(Console.ReadLine());
+            while (nAv2 < 0.0 & nAv2 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                nAv2 = int.Parse(Console.ReadLine());
+
+            }
+            // calculo
+
+
+
+
+
+
         }
     }
 }
---
+++ Exercicios_Udemy/Vetor/Vetor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (int i = 0; i<N; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i<N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("f1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
