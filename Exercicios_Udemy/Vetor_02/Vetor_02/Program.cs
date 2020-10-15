50033d2d673f15b3cc761f907de854772f2d41ea
Bsaltori <biasaltori@yahoo.com.br>
2020-10-15 11:30:07 -0300

    Vetor
    Exercicios de vetor

---
+++ Exercicios_Udemy/Vetor_02/Vetor_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] A;
+
+            N = int.Parse(Console.ReadLine());
+            A = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i=0; i<N; i++)
+            {
+                A[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+
+            }
+            for (int i=0; i<N; i++)
+            {
+                Console.Write(A[i].ToString("f1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            double soma = 0.0;
+
+            for (int i=0; i< N; i++)
+            {
+                soma = soma + A[i];
+            }
+
+            double media = soma / N;
+
+            Console.WriteLine(soma.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
+          
+
+
+
+        }
+    }
+}
---
+++ Exercicios_Udemy/Vetor_1/Vetor_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Vetor_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i<N; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i=0; i<N; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+        }
+    }
+}
