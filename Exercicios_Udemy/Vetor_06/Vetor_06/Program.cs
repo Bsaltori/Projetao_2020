529eacb1970acbcc765f426ce0a3b4ce32c01f35
Bsaltori <biasaltori@yahoo.com.br>
2020-10-28 20:50:33 -0300

    Vetor
    Exercicios de vetor

---
+++ Exercicios_Udemy/Vetor_06/Vetor_06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Vetor_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+            int[] B = new int[N];
+
+            string[] X = Console.ReadLine().Split(" ");
+            for(int i =0; i<N; i++)
+            {
+                A[i] = int.Parse(X[i]);
+            }
+
+            string[] Y = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = int.Parse(Y[i]);
+            }
+
+            int[] C = new int[N];
+
+            int soma = A[0] + B[0];
+
+
+
+
+            for (int i = 0; i<N; i++)
+            {
+                soma = A[i] + B[i];
+                C[i] = soma;
+                
+
+                Console.Write(C[i] + " ");
+               
+                
+            }
+
+            
+        }
+           
+    }
+}
--- Exercicios_Udemy/Vetor_5/Vetor_5/Program.cs
+++ Exercicios_Udemy/Vetor_5/Vetor_5/Program.cs
@@ -10,15 +10,37 @@ namespace Vetor_5
             int[] vet = new int[N];
 
             string[] numeros = Console.ReadLine().Split(' ');
+
             for (int i = 0; i < N; i++)
             {
                 vet[i] = int.Parse(numeros[i]);
             }
 
+            //numeros pares
             for (int i = 0; i<N; i++)
             {
-               if(vet[i] == )
+               if (vet[i] % 2 == 0)
+                {
+                    Console.Write(vet[i] + " ");
+
+                    }
             }
+
+            Console.WriteLine();
+
+            //quantidade de pares
+            int pares = 0;
+            for(int i = 0; i<N; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    pares = i;
+                }
+            }
+
+            Console.WriteLine(pares);
+            
         }
+
     }
 }
